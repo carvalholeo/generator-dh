c8efa411f5ed201ae10bf8aab1cfa823639701a5
Leo Carvalho <carvalho.csleo@gmail.com>
2021-05-17 00:37:00 -0300

    :white_check_mark: Try to fix test to silent option

--- test/integration/silent.spec.js
+++ test/integration/silent.spec.js
@@ -29,31 +29,32 @@ describe('express-dh(1)', function () {
     let output = ''
 
     it('should create basic app with all dependencies installed', function (done) {
-      runRaw(ctx.dir, ['--silent'], function (err, code, stdout, stderr) {
-        if (err || stderr) {
+      runRaw(ctx.dir, ['--silent'], function (err, code, stdout) {
+        if (err) {
           return done(err)
         }
 
         output += stdout
 
         ctx.files = parseCreatedFiles(stdout, ctx.dir)
+        console.log(ctx.files)
         strictEqual(ctx.files.length, 17, 'should have 17 files')
+        console.log(ctx.files)
 
         ok(/ instalando dependências do NPM/.test(stdout))
         ok(/ inicializando repositório Git/.test(stdout))
         ok(/ adicionando arquivos ao Git/.test(stdout))
         ok(/ fazendo primeiro commit dos arquivos/.test(stdout))
         ok(/ Instalação concluída!/.test(stdout))
+        console.log(ctx.files)
 
-        return done()
+        done()
       })
     })
 
     it('should have basic files', function () {
       const currentFiles = readdirSync(ctx.dir)
       const files = parseCreatedFiles(output, ctx.dir)
-      console.log(files)
-      console.log(output)
 
       notStrictEqual(files.indexOf('bin/www'), -1, 'should have bin/www file')
       notStrictEqual(files.indexOf('app.js'), -1, 'should have app.js file')
