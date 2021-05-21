d381b9ffed5448f05ba410f361b80f205a6fc644
Leo Carvalho <carvalho.csleo@gmail.com>
2021-05-21 15:34:03 -0300

    :racehorse: Minify files from dist folder

--- dist/utils/warning.js
+++ dist/utils/warning.js
@@ -1,17 +1,1 @@
-"use strict";
-
-/**
- * Display a warning similar to how errors are displayed by commander.
- *
- * @param {String} message Message to be prompted for the user
- * @return {void}
- */
-function warning(message) {
-  console.error();
-  message.split('\n').forEach(function (line) {
-    console.error('  warning: %s', line);
-  });
-  console.error();
-}
-
-module.exports = warning;
+"use strict";function warning(message){console.error();message.split("\n").forEach(function(line){console.error("  warning: %s",line)});console.error()}module.exports=warning;
