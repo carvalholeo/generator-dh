5a94ef143e11060661d36d4ad1ffa661f6413321
Leo Carvalho <carvalho.csleo@gmail.com>
2021-05-21 15:34:03 -0300

    :racehorse: Minify files from dist folder

--- dist/utils/launchedFromCmd.js
+++ dist/utils/launchedFromCmd.js
@@ -1,11 +1,1 @@
-"use strict";
-
-/**
- * Determine if launched from cmd.exe
- * @return {Boolean} Return true if the process were lauched from PowerShell or Windows CMD
- */
-function launchedFromCmd() {
-  return process.platform === 'win32' && process.env._ === undefined;
-}
-
-module.exports = launchedFromCmd;
+"use strict";function launchedFromCmd(){return process.platform==="win32"&&process.env._===undefined}module.exports=launchedFromCmd;
