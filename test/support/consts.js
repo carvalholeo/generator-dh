1c5548084c4b11e7d5a37f71d63d46bb22bd802e
Leo Carvalho <carvalho.csleo@gmail.com>
2021-07-14 17:28:16 -0300

    :art: Change binary command to tests suite

--- test/support/consts.js
+++ test/support/consts.js
@@ -4,7 +4,7 @@ const tmpDir = require('./utils/tmpDir')
 const PKG_PATH = resolve(__dirname, '..', '..', 'package.json')
 
 const TEMP_DIR = tmpDir()
-const BIN_PATH = resolve(dirname(PKG_PATH), require(PKG_PATH).bin['express-dh'])
+const BIN_PATH = resolve(dirname(PKG_PATH), require(PKG_PATH).bin['express-generator-dh'])
 const APP_START_STOP_TIMEOUT = 10000
 const NPM_INSTALL_TIMEOUT = 300000 // 5 minutes
 const TIME_SLEEP = 3000
