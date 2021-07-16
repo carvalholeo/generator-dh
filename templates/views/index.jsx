4ba4205b2a347a28f6cad7d07fdef0e5ea03e770
Leo Carvalho <carvalho.csleo@gmail.com>
2021-07-16 19:41:43 -0300

    :rocket: Add JSX templates

---
+++ templates/views/index.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+const Layout = require('./layout');
+
+function Index(props) {
+  return (
+    <Layout title={props.title}>
+      <h1>{props.title}</h1>
+      <p>Welcome to {props.title}</p>
+    </Layout>
+  );
+}
+
+module.exports = Index;
